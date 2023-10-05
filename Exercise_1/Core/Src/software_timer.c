440d739379ba358984094eaa795cc51b797940e2
hiepdoan78 <hiepdoan7820@gmail.com>
2023-10-05 09:39:03 +0700

    first commit

---
+++ Exercise_1/Core/Src/software_timer.c
@@ -0,0 +1,23 @@
+/*
+ * software_timer.c
+ *
+ *  Created on: Oct 2, 2023
+ *      Author: Hiepm
+ */
+
+int timer_counter = 0;
+int timer_flag = 0;
+
+void setTimer(int duration){
+	timer_counter = duration;
+	timer_flag = 0;
+}
+
+void timerRun(){
+	if(timer_counter > 0){
+		timer_counter--;
+		if(timer_counter <= 0){
+			timer_flag = 1;
+		}
+	}
+}
